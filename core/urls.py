cdd6cdc4ce003dd795ac61c0a659282c0902f609
chidozie <dozie.steve@gmail.com>
2024-01-03 18:14:21 +0100

    file-chat url and view update

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ app_name = 'core'
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('data-upload/', views.DataUploadView.as_view(), name='data-upload'),
+    path('convo/<conversation_id>/', views.FileChatView.as_view(), name='file-chat'),
 ]
