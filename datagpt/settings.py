5969bf4df39d7be967cc5289edb70a262bcdb9ea
chidozie <dozie.steve@gmail.com>
2024-01-05 04:20:17 +0100

    fix chat image

--- datagpt/settings.py
+++ datagpt/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [str(BASE_DIR / "static")]
 MEDIA_ROOT = BASE_DIR / 'attachments'
 MEDIA_URL = '/media/'
 FIG_ROOT = BASE_DIR / 'images'
-FIG_URL = '/media/'
+FIG_URL = '/fig/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
