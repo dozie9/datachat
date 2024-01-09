a6b3cb8f36fa427066e275d860518e154a1d05a3
chidozie <dozie.steve@gmail.com>
2024-01-09 10:14:51 +0100

    update home

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.db.models import Max
 
 from core.models import Conversation
 
@@ -6,4 +7,8 @@ from core.models import Conversation
 def get_conversations(_request):
     if not _request.user.is_authenticated:
         return
-    return {'CONVERSATIONS': Conversation.objects.filter(user1=_request.user).order_by('-updated_at')}
+    conversation_qs = Conversation.objects.filter(user1=_request.user).alias(
+        latest_message=Max('message__timestamp')
+    ).order_by('-latest_message')
+
+    return {'CONVERSATIONS': conversation_qs}
